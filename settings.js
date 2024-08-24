b5b541ce9f752f077d22f254db3da194edb293eb
Kara-kakashi <faty92851@gmail.com>
2024-08-24 18:11:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "221706691940"
-global.ownername = "꧁༆~𝑲𝑨𝑹𝑨~𝑲𝑨𝑲𝑨𝑺𝑯𝑰༆꧂"
+global.ownernomer = "263717750825"
+global.ownername = "Precious Ayomide👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
